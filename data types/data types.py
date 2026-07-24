f5175309b72b56c1cf86cfa956663f2750c03806
jipunroy95 <jipunroy95@gmail.com>
2026-07-24 10:11:42 +0600

    data type add

---
+++ data types/data types.py
@@ -0,0 +1,44 @@
+"""# Numeric Data Type
+x = 100
+y = -50
+
+print(type(x))
+
+# Float
+price = 25.10
+
+print(type(price))
+
+# Arithmetic Example
+a = 20
+b = 5
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a % b)
+print(a**b)
+
+# String Indexing
+text = "Python"
+
+print(text[0])
+print(text[1])
+print(text[-1])
+
+# String Slicing
+
+# String Methods Upper
+name = "python"
+
+print(name.upper())
+
+# String Methods Lower
+name = "python"
+
+print(name.lower())
+"""
+# Replace
+print("Python".replace("Python", "Java"))
