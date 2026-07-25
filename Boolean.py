f2d53adca7ad0fdf7ffdcd9fe603883615af0992
jipunroy95 <jipunroy95@gmail.com>
2026-07-25 18:27:46 +0600

    update

---
+++ Boolean.py
@@ -0,0 +1,38 @@
+# age = 20
+
+# if age >= 18:
+#     print("Adult")
+# else:
+#     print("Minor")
+
+# any() যদি অন্তত একটি True থাকে
+# numbers = [False, False, True]
+
+# print(any(numbers))
+
+# all() সবগুলো True হতে হবে।
+# numbers = [True, True, True]
+
+# print(all(numbers))
+
+# numbers = [True, False]
+
+# print(all(numbers))
+
+# Boolean in Loops
+# running = True
+
+# while running:
+
+#     print("Running...")
+#     running = False
+
+# Real-Life Example (Login System)
+username = "Admin"
+password = "1234"
+
+if username == "admin" and password == "1234":
+    print("Login Successful")
+
+else:
+    print("Wrong Credentials")
---
+++ Python String Indexing.py
@@ -0,0 +1,33 @@
+# Positive Indexing
+"""""
+text = "Python"
+
+print(text[0])
+print(text[1])
+print(text[5])
+"""
+# Negative Indexing
+"""
+text = "Python"
+
+print(text[-1])
+print(text[-2])
+print(text[-6])
+"""
+
+# Accessing Any Character
+# name = "Bangladesh"
+
+# print(name[4])
+# print(name[-3])
+
+text = "Python"
+
+print(text[0])
+print(text[1])
+print(text[5])
+
+# Basic Slicing
+# text = "Python"
+
+# print(text[0:2])
